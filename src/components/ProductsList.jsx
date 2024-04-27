4ae0bfa2e83965a9e315e37d130211a2e21d10d2
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2024-04-27 22:37:18 +0500

    add new

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -5,7 +5,16 @@ import { Link } from "react-router-dom"
 function ProductsList() {
   return (
     <div>
-      
+      <div className="card card-side bg-base-100 shadow-xl">
+  <figure><img src="" alt="Movie"/></figure>
+  <div className="card-body flex flex-col items-center justify-center">
+    <h2 className="card-title">Honor</h2>
+    <p>100$</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Remove</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
